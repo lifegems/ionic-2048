2f89bfc95756a8d56d09759791f1c9b9171c2424
Michalik, Joshua <jsmichal@usa.wtbts.net>
2018-03-09 17:44:40 -0500

    Add new library code for game

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,18 +1,23 @@
 import { Action, ActionReducer } from '@ngrx/store';
 import * as actions from './app.actions';
 import * as game from '../lib/game';
+import { Game2048, Direction } from '../lib/game2048';
 
 interface State {
-  grid: Number[];
+  grid: Array<number>;
   points: number;
   gameover: any;
 }
 
+// let MyGame: Game2048 = new Game2048();
+// MyGame.newGame();
+
 const initState: State = {
-  grid: game.setupInitialCells(),
+  grid: game.setupInitialCells(), // MyGame.getGridArray(),
   points: 0,
   gameover: null,
 };
+
 
 export const appReducer: ActionReducer<State, Action> = (state: State = initState, action: Action) => {
   switch(action.type) {
@@ -20,6 +25,9 @@ export const appReducer: ActionReducer<State, Action> = (state: State = initStat
       return initState;
     }
     case actions.SWIPE_LEFT: {
+      // MyGame.merge(Direction.Left);
+      // let grid = MyGame.getGridArray();
+      // return {...state, grid};
       let grid = [...state.grid];
       grid = game.merge(grid, 'LEFT');
       let points = state.points + game.getRoundScore(grid, state.grid);
@@ -30,6 +38,9 @@ export const appReducer: ActionReducer<State, Action> = (state: State = initStat
       return {...state, grid, points, gameover};
     }
     case actions.SWIPE_RIGHT: {
+      // MyGame.merge(Direction.Right);
+      // let grid = MyGame.getGridArray();
+      // return {...state, grid};
       let grid = [...state.grid];
       grid = game.merge(grid, 'RIGHT');
       let points = state.points + game.getRoundScore(grid, state.grid);
@@ -40,6 +51,9 @@ export const appReducer: ActionReducer<State, Action> = (state: State = initStat
       return {...state, grid, points, gameover};
     }
     case actions.SWIPE_UP: {
+      // MyGame.merge(Direction.Up);
+      // let grid = MyGame.getGridArray();
+      // return {...state, grid};
       let grid = [...state.grid];
       grid = game.merge(grid, 'UP');
       let points = state.points + game.getRoundScore(grid, state.grid);
@@ -50,6 +64,9 @@ export const appReducer: ActionReducer<State, Action> = (state: State = initStat
       return {...state, grid, points, gameover};
     }
     case actions.SWIPE_DOWN: {
+      // MyGame.merge(Direction.Down);
+      // let grid = MyGame.getGridArray();
+      // return {...state, grid};
       let grid = [...state.grid];
       grid = game.merge(grid, 'DOWN');
       let points = state.points + game.getRoundScore(grid, state.grid);
