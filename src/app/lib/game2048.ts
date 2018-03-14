66b362f7424e14aaa593bdcddba9dcf1e440f606
Michalik, Joshua <jsmichal@usa.wtbts.net>
2018-03-14 10:38:23 -0400

    Replace game functions with game object

--- src/app/lib/game2048.ts
+++ src/app/lib/game2048.ts
@@ -1,50 +1,213 @@
-import { Grid } from './grid';
-
 export enum Direction {
-  Up,
-  Down,
-  Left,
-  Right
+  Up, Down, Left, Right
 }
 
+const DEFAULT_GRID: string = "16";
+
 export class Game2048 {
-  private grid: Grid;
+  private grid: number[] = [];
+  private points: number = 0;
+  private GAMEOVER_STATE: boolean = null;
 
-  /**
-   * Establish a grid, but don't start a game. See newGame().
-   */
-  constructor() {}
-
-  public newGame() {
-    this.grid = new Grid(4, 4);
-    this.grid.addNewNumber();
-    this.grid.addNewNumber();
+  constructor(grid: string = DEFAULT_GRID) {
+    if (grid === DEFAULT_GRID || grid.length !== 16) {
+      this.grid = Array(16).fill(0);
+      this.addNumberToGrid();
+      this.addNumberToGrid();
+    } else {
+      for (let i = 0; i < grid.length; i++) {
+        this.grid.push(Number(grid[i]));
+      }
+    }
   }
 
-  public getGridArray(): Array<number> {
-    return this.grid.toFlatArray();
+  public getBoardArray(): number[] {
+    return this.grid;
+  }
+
+  public getBoardAscii(): string {
+    return `
+    ${this.grid[0]} ${this.grid[1]} ${this.grid[2]} ${this.grid[3]}
+    ${this.grid[4]} ${this.grid[5]} ${this.grid[6]} ${this.grid[7]}
+    ${this.grid[8]} ${this.grid[9]} ${this.grid[10]} ${this.grid[11]}
+    ${this.grid[12]} ${this.grid[13]} ${this.grid[14]} ${this.grid[15]}
+    `;
+  }
+
+  public getBoardString(): string {
+    let grid = "";
+    this.grid.forEach(i => grid += i);
+    return grid;
+  }
+
+  public addNumberToGrid(): void {
+    let randomIndex = this.getRandomEmptyIndex();
+    if (randomIndex > -1) {
+      let randomDigit = Math.floor(Math.random() * 2) + 1;
+      this.grid[randomIndex] = randomDigit * 2;
+    }
+  }
+
+  public GameOver(): boolean {
+    return this.GAMEOVER_STATE;
+  }
+
+  public move(direction: Direction): void {
+    let grid = [...this.getBoardArray()];
+    let oldGridString = this.getBoardString();
+    switch(direction) {
+      case Direction.Left: {
+        grid = this.setRow(grid, 0, this.squashSet(this.getRow(grid, 0)));
+        grid = this.setRow(grid, 1, this.squashSet(this.getRow(grid, 1)));
+        grid = this.setRow(grid, 2, this.squashSet(this.getRow(grid, 2)));
+        grid = this.setRow(grid, 3, this.squashSet(this.getRow(grid, 3)));
+        break;
+      };
+      case Direction.Right: {
+        grid = this.setRow(grid, 0, this.squashSet(this.getRow(grid, 0).reverse()).reverse());
+        grid = this.setRow(grid, 1, this.squashSet(this.getRow(grid, 1).reverse()).reverse());
+        grid = this.setRow(grid, 2, this.squashSet(this.getRow(grid, 2).reverse()).reverse());
+        grid = this.setRow(grid, 3, this.squashSet(this.getRow(grid, 3).reverse()).reverse());
+        break;
+      };
+      case Direction.Up: {
+        grid = this.setCol(grid, 0, this.squashSet(this.getCol(grid, 0)));
+        grid = this.setCol(grid, 1, this.squashSet(this.getCol(grid, 1)));
+        grid = this.setCol(grid, 2, this.squashSet(this.getCol(grid, 2)));
+        grid = this.setCol(grid, 3, this.squashSet(this.getCol(grid, 3)));
+        break;
+      };
+      case Direction.Down: {
+        grid = this.setCol(grid, 0, this.squashSet(this.getCol(grid, 0).reverse()).reverse());
+        grid = this.setCol(grid, 1, this.squashSet(this.getCol(grid, 1).reverse()).reverse());
+        grid = this.setCol(grid, 2, this.squashSet(this.getCol(grid, 2).reverse()).reverse());
+        grid = this.setCol(grid, 3, this.squashSet(this.getCol(grid, 3).reverse()).reverse());
+        break;
+      };
+    }
+    this.points += this.getRoundScore(grid, this.grid);
+    this.grid = grid;
+    if (oldGridString == this.getBoardString()) {
+      if (this.isLoss(this.grid)) {
+        this.GAMEOVER_STATE = false;
+      }
+      return;
+    }
+    this.addNumberToGrid();
   }
 
   public getPoints() {
-    return 0;
+    return this.points;
   }
 
-  public merge(direction: Direction) {
-    switch (direction) {
-      case Direction.Up:
-        this.grid.mergeGridUp();
-        break;
-      case Direction.Down:
-        this.grid.mergeGridDown();
-        break;
-      case Direction.Left:
-        this.grid.mergeGridLeft();
-        break;
-      case Direction.Right:
-        this.grid.mergeGridRight();
-        break;
-      default:
-        break;
+  private getNumberScore(gridNew, gridOld, digit) {
+    let count = gridNew.filter(c => c === digit).length - gridOld.filter(c => c === digit).length;
+    return (count > 0) ? count * digit : 0;
+  }
+
+  private getRoundScore(gridNew, gridOld) {
+    let score = 0;
+    score += this.getNumberScore(gridNew, gridOld, 4);
+    score += this.getNumberScore(gridNew, gridOld, 8);
+    score += this.getNumberScore(gridNew, gridOld, 16);
+    score += this.getNumberScore(gridNew, gridOld, 32);
+    score += this.getNumberScore(gridNew, gridOld, 64);
+    score += this.getNumberScore(gridNew, gridOld, 128);
+    score += this.getNumberScore(gridNew, gridOld, 512);
+    score += this.getNumberScore(gridNew, gridOld, 1024);
+    score += this.getNumberScore(gridNew, gridOld, 2048);
+    return score;
+  }
+
+  private getCol(grid, intIndex) {
+    return [
+      grid[0 + intIndex],
+      grid[4 + intIndex],
+      grid[8 + intIndex],
+      grid[12 + intIndex],
+    ]
+  }
+
+  private getRow(grid: number[], intIndex) {
+    let rowItem = 4 * intIndex;
+    return [
+      grid[0 + rowItem],
+      grid[1 + rowItem],
+      grid[2 + rowItem],
+      grid[3 + rowItem],
+    ];
+  }
+
+  private getRandomEmptyIndex(): number {
+    let aEmptyIndex = [];
+    for (let i = 0; i < this.grid.length; i++) {
+      if (this.grid[i] === 0) {
+        aEmptyIndex.push(i);
+      }
     }
+    if (aEmptyIndex.length > 0) {
+      let randomIndex = Math.floor(Math.random() * (aEmptyIndex.length + 1));
+      return aEmptyIndex[randomIndex];
+    }
+    return -1;
+  }
+
+  private isLoss(g: number[]): boolean {
+    if (g.filter(c => c === 0).length === 0) {
+      if (g[0] === g[1] || g[0] === g[4]
+        || g[1] === g[2] || g[1] === g[5]
+        || g[2] === g[3] || g[2] === g[6]
+                        || g[3] === g[7]
+        || g[4] === g[5] || g[4] === g[8]
+        || g[5] === g[6] || g[5] === g[9]
+        || g[6] === g[7] || g[6] === g[10]
+                        || g[7] === g[11]
+        || g[8] === g[9] || g[8] === g[12]
+        || g[9] === g[10] || g[9] === g[13]
+        || g[10] === g[11] || g[10] === g[14]
+                          || g[11] === g[15]
+        || g[12] === g[13]
+        || g[13] === g[14]
+        || g[14] === g[15]) {
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private setCol(grid: number[], intIndex: number, cells: number[]) {
+    grid[intIndex + 0] = cells[0];
+    grid[intIndex + 4] = cells[1];
+    grid[intIndex + 8] = cells[2];
+    grid[intIndex + 12] = cells[3];
+    return grid;
+  }
+
+  private setRow(grid: number[], intIndex: number, cells: number[]) {
+    grid[intIndex * 4 + 0] = cells[0];
+    grid[intIndex * 4 + 1] = cells[1];
+    grid[intIndex * 4 + 2] = cells[2];
+    grid[intIndex * 4 + 3] = cells[3];
+    return grid;
+  }
+
+  private squashSet(cells: number[]): number[] {
+    cells = cells.filter(i => i !== 0);
+    cells.push(...Array(4 - cells.length).fill(0));
+    let [cellA, cellB, cellC, cellD] = cells;
+
+    let newSet: number[] = [cellA, cellB, cellC, cellD];
+    if (cellA === cellB && cellC === cellD) {
+      newSet = [cellA + cellB, cellC + cellD];
+    } else if (cellA === cellB) {
+      newSet = [cellA + cellB, cellC, cellD];
+    } else if (cellB === cellC) {
+      newSet = [cellA, cellB + cellC, cellD];
+    } else if (cellC === cellD) {
+      newSet = [cellA, cellB, cellC + cellD];
+    }
+    newSet.push(...Array(4 - newSet.length).fill(0));
+    return newSet;
   }
 }
--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,6 +1,5 @@
 import { Action, ActionReducer } from '@ngrx/store';
 import * as actions from './app.actions';
-import * as game from '../lib/game';
 import { Game2048, Direction } from '../lib/game2048';
 
 interface State {
@@ -9,11 +8,10 @@ interface State {
   gameover: any;
 }
 
-// let MyGame: Game2048 = new Game2048();
-// MyGame.newGame();
+let MyGame: Game2048 = new Game2048();
 
 const initState: State = {
-  grid: game.setupInitialCells(), // MyGame.getGridArray(),
+  grid: MyGame.getBoardArray(),
   points: 0,
   gameover: null,
 };
@@ -25,56 +23,40 @@ export const appReducer: ActionReducer<State, Action> = (state: State = initStat
       return initState;
     }
     case actions.SWIPE_LEFT: {
-      // MyGame.merge(Direction.Left);
-      // let grid = MyGame.getGridArray();
-      // return {...state, grid};
-      let grid = [...state.grid];
-      grid = game.merge(grid, 'LEFT');
-      let points = state.points + game.getRoundScore(grid, state.grid);
-      if (!game.doGridsMatch(grid, state.grid)) {
-        grid = game.addRandomNumber(grid);
-      }
-      let gameover = game.getEndGameState(grid);
-      return {...state, grid, points, gameover};
+      MyGame.move(Direction.Left);
+      return {
+        ...state,
+        grid: MyGame.getBoardArray(),
+        gameover: MyGame.GameOver(),
+        points: MyGame.getPoints(),
+      };
     }
     case actions.SWIPE_RIGHT: {
-      // MyGame.merge(Direction.Right);
-      // let grid = MyGame.getGridArray();
-      // return {...state, grid};
-      let grid = [...state.grid];
-      grid = game.merge(grid, 'RIGHT');
-      let points = state.points + game.getRoundScore(grid, state.grid);
-      if (!game.doGridsMatch(grid, state.grid)) {
-        grid = game.addRandomNumber(grid);
-      }
-      let gameover = game.getEndGameState(grid);
-      return {...state, grid, points, gameover};
+      MyGame.move(Direction.Right);
+      return {
+        ...state,
+        grid: MyGame.getBoardArray(),
+        gameover: MyGame.GameOver(),
+        points: MyGame.getPoints(),
+      };
     }
     case actions.SWIPE_UP: {
-      // MyGame.merge(Direction.Up);
-      // let grid = MyGame.getGridArray();
-      // return {...state, grid};
-      let grid = [...state.grid];
-      grid = game.merge(grid, 'UP');
-      let points = state.points + game.getRoundScore(grid, state.grid);
-      if (!game.doGridsMatch(grid, state.grid)) {
-        grid = game.addRandomNumber(grid);
-      }
-      let gameover = game.getEndGameState(grid);
-      return {...state, grid, points, gameover};
+      MyGame.move(Direction.Up);
+      return {
+        ...state,
+        grid: MyGame.getBoardArray(),
+        gameover: MyGame.GameOver(),
+        points: MyGame.getPoints(),
+      };
     }
     case actions.SWIPE_DOWN: {
-      // MyGame.merge(Direction.Down);
-      // let grid = MyGame.getGridArray();
-      // return {...state, grid};
-      let grid = [...state.grid];
-      grid = game.merge(grid, 'DOWN');
-      let points = state.points + game.getRoundScore(grid, state.grid);
-      if (!game.doGridsMatch(grid, state.grid)) {
-        grid = game.addRandomNumber(grid);
-      }
-      let gameover = game.getEndGameState(grid);
-      return {...state, grid, points, gameover};
+      MyGame.move(Direction.Down);
+      return {
+        ...state,
+        grid: MyGame.getBoardArray(),
+        gameover: MyGame.GameOver(),
+        points: MyGame.getPoints(),
+      };
     }
     default:
       return {...state};
